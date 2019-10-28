f034f5c72d798588da34173f8a98a7faa24dc191
Michelle Johnson <bob1234girl@gmail.com>
2019-10-28 13:51:23 -0500

    added .find example from chapter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -241,3 +241,26 @@ const agentsWithCompanyAndPhone = businesses.map(business => {
     outEl.innerHTML += `<p>Phone: ${agent.phoneNumber}</p>`
     outEl.innerHTML += "<hr/>";
   });
+
+
+
+
+  const candies = [
+    {
+        name: "Lollipop",
+        price: 2.99
+    },
+    {
+        name: "Tootsie Roll",
+        price: 1.49
+    },
+    {
+        name: "Sugar Daddy",
+        price: 2.49
+    }
+]
+
+const firstCheapCandy = candies.find(candy => candy.price < 2.00)
+
+console.log(firstCheapCandy)
+> { name: "Tootsie Roll", price: 1.49 }
